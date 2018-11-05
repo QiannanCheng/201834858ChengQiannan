033e9455f10e0e21a968dab85cfa940f440a8834
QiannanCheng <2776294265@qq.com>
2018-11-05 14:34:18 +0800

    update n-foldCrossValidation

--- Homework1/KNN/n-foldCrossValidation.py
+++ Homework1/KNN/n-foldCrossValidation.py
@@ -39,7 +39,7 @@ for i in range(5): #i=0,1,2,3,4
         LabelsList.append(trainLabels[i*num:i*num+num])
         
 #5折交叉验证，得到使平均错误率最小的k值
-kList=list(range(5,101,5)) #得到k的取值列表[5,10,15,20,...,100]
+kList=list(range(10,51,10)) #得到k的取值列表[10,20,30,40,50]
 averageList=[] #k对应的平均错误率
 minErrorRate=six.MAXSIZE
 for k in kList:
@@ -54,10 +54,18 @@ for k in kList:
         #合并得到训练矩阵对应的类标签
         traLabels=LabelsList[idx[0]]+LabelsList[idx[1]]+LabelsList[idx[2]]+LabelsList[idx[3]]     
         er=kNN.errorRate(valMat,valLabels,traMat,traLabels,k) #得到错误率
+        #record
+        recordf=open('record.txt','a')
+        recordf.write('ErrorRate='+str(er)+'\n')
+        recordf.close()
         ersum+=er 
     average=ersum/5
     averageList.append(average) #记录k值对应的平均错误率
     print("k=%d, AverageErrorRate=%f" % (k,average))
+    #record
+    recordf=open('record.txt','a')
+    recordf.write('k='+str(k)+', AverageErrorRate='+str(average)+'\n')
+    recordf.close()
     if average<minErrorRate:
         minErrorRate=average
         opk=k
@@ -69,12 +77,24 @@ plt.legend()
 plt.xlabel('k Value')
 plt.ylabel('Average Error Rate')
 plt.show()
-#print
+#print & record
 print("Optimal k: %d, AverageErrorRate: %f" % (opk,minErrorRate))
+recordf=open('record.txt','a')
+recordf.write(str(kList))
+recordf.write(str(averageList))
+recordf.write('Optimal k: '+str(opk)+', AverageErrorRate: '+str(minErrorRate)+'\n')
+recordf.close()
 #将最优k值在测试数据上进行测试
 testMat,testLabels=kNN.file2matrix("testData.txt")
 result=kNN.errorRate(testMat,testLabels,trainMat,trainLabels,opk)
 print("errorRate on testData: "+str(result))
+#record
+recordf.open('record.txt','a')
+recordf.write('ErrorRate on testData: '+str(result))
+recordf.close()
 print("n-fold Cross Validation Finished!")
+
+
+
 
 
